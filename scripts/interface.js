7f1a0faef5dae9c8d2694483a98256f958ae0ae0
Hebert324 <hebertrocha76@gmail.com>
2021-05-19 11:31:51 -0300

    Update interface.js

--- scripts/interface.js
+++ scripts/interface.js
@@ -21,19 +21,3 @@ function handleClick(event) {
     //e aqui rodamos outra função que vai atualizar nossos objetos colocando o escudo ou a as espadas.
     updateSquares()
 }
-function updateSquares(){
-    //aqui nos fazemos igual la em cima pegamos todos os elementos square e adicionamos ele a uma variavel squares.
-    let squares = document.querySelectorAll(".square")
-
-    //depois ele vai fazer um forEach() que vai executar uma função em cada um dos elemento do squares.
-    squares.forEach((square)=>{
-        //aqui nos pegamos o id do square que recebeu o target acima e vamos chama-lo de position.
-        let position = square.id
-        //depois vamos pegar a variavel symbol que esta la no game.js, aonde ele contem o: O e o X, que serão adicionados aos elementos como uma class.
-        let symbol = board[position]
-        //depois fazemos uma verificação, se o symbol do elemento estiver vazio ele vai adicionar uma div que tem a classe com o symbol que e o: O ou o X
-        if(symbol != ''){
-            square.innerHTML = `<div class='${symbol}'></div>`
-        }
-    })
-}
