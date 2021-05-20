1e602be0dbe59a467148085c8080eb1116e14c14
Hebert324 <hebertrocha76@gmail.com>
2021-05-20 08:52:22 -0300

    game Over

--- scripts/game.js
+++ scripts/game.js
@@ -2,19 +2,27 @@
 let board = ['', '', '', '', '', '', '', '', '']
 let playerTime = 0
 let symbols = ['o', 'x']
+let gameOver = false
 
 //no handleMove ele pega a posição que passamos do evento la do interface.js e pega a posição do board que passamos e adiciona a ele um symbol que começa na posição 0 de acordo com o playerTime
 function handleMove(position){
+
+    //aqui nos verificamos se o jogo acabou
+    if(gameOver){
+        return
+    }
+
     //aqui ele vai verificar se o elelemnto e igual a vazio para que eu não consiga mudar o elemento que ja foi colocado.
     if(board[position] == ''){
         board[position] = symbols[playerTime]
-    }
 
-    //depois fazemos uma verificação com if para que se o playerTime for igual a 0 ele mude para 1 e se for igual a 1 ele mude para 0
-    if(playerTime == 0){
-        playerTime = 1
-    }
-    else{
-        playerTime = 0
+        //depois fazemos uma verificação com if para que se o playerTime for igual a 0 ele mude para 1 e se for igual a 1 ele mude para 0
+
+        if(playerTime == 0){
+            playerTime = 1
+        }
+        else{
+            playerTime = 0
+        }  
     }
 }
